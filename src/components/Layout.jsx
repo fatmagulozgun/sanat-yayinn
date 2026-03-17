b714726d57aa3aa45af75ce3c1afd403bad142ea
Fatmagül Özgün <ftmglozgn@gmail.com>
2026-03-17 16:51:16 +0300

    duzen

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+
 const Layout = () => {
+  const location = useLocation();
+  const canonicalUrl = `https://sanat-yayinn.vercel.app${location.pathname}`;
+
   return (
     <div className="app">
+      <Helmet>
+        <link rel="canonical" href={canonicalUrl} />
+      </Helmet>
       <Navbar />
       <main className="main-content">
         <Outlet />
